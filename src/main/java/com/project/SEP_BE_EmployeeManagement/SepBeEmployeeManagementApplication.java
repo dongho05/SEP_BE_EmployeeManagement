ad28e6594085e1f726dcd47c46eb467aaaebaf65
dong.ho <hophuongdong25052001@gmail.com>
2023-09-18 12:52:00 +0700

    initialize

---
+++ src/main/java/com/project/SEP_BE_EmployeeManagement/SepBeEmployeeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.project.SEP_BE_EmployeeManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SepBeEmployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SepBeEmployeeManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/SEP_BE_EmployeeManagement/SepBeEmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.SEP_BE_EmployeeManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SepBeEmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
