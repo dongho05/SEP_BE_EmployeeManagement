ad56fe7677ec79304a2e50acb4cc934170b4ac13
Nguyen Dinh Phu <phunguyen06072001@gmail.com>
2023-10-04 08:56:26 +0700

    thêm cross

--- src/main/java/com/project/SEP_BE_EmployeeManagement/controller/LoginController.java
+++ src/main/java/com/project/SEP_BE_EmployeeManagement/controller/LoginController.java
@@ -28,6 +28,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class LoginController {
     @Autowired
     private UserService userService;
