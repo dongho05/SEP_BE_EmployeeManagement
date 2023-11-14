5bc8b159fb1074d91bf7d37a297758a4dfe2bce7
dong.ho <hophuongdong25052001@gmail.com>
2023-11-14 21:02:08 +0700

    update docker-compose, Dockerfile

--- src/main/java/com/project/SEP_BE_EmployeeManagement/config/Swagger2Config.java
+++ src/main/java/com/project/SEP_BE_EmployeeManagement/config/Swagger2Config.java
@@ -11,24 +11,24 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
-@EnableSwagger2
+//@Configuration
+//@EnableSwagger2
 public class Swagger2Config {
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.project.SEP_BE_EmployeeManagement"))
-                .paths(PathSelectors.regex("/.*"))
-                .build()
-                .apiInfo(apiEndPointsInfo());
-    }
-
-    private ApiInfo apiEndPointsInfo() {
-        return new ApiInfoBuilder()
-                .title("Employee Management System")
-                .description("Employee Management System REST API")
-                .contact(new Contact("SEP490-G79", "", "donghphe150112@fpt.edu.vn"))
-                .version("1.0.0")
-                .build();
-    }
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2).select()
+//                .apis(RequestHandlerSelectors.basePackage("com.project.SEP_BE_EmployeeManagement"))
+//                .paths(PathSelectors.regex("/.*"))
+//                .build()
+//                .apiInfo(apiEndPointsInfo());
+//    }
+//
+//    private ApiInfo apiEndPointsInfo() {
+//        return new ApiInfoBuilder()
+//                .title("Employee Management System")
+//                .description("Employee Management System REST API")
+//                .contact(new Contact("SEP490-G79", "", "donghphe150112@fpt.edu.vn"))
+//                .version("1.0.0")
+//                .build();
+//    }
 }
