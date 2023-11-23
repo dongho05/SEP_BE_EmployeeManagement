b22edb68458151a9a88745841701024546203568
dong.ho <hophuongdong25052001@gmail.com>
2023-11-23 11:34:47 +0700

    edit AttendanceController

--- src/main/java/com/project/SEP_BE_EmployeeManagement/controller/AttendanceController.java
+++ src/main/java/com/project/SEP_BE_EmployeeManagement/controller/AttendanceController.java
@@ -2,13 +2,18 @@ package com.project.SEP_BE_EmployeeManagement.controller;
 
 import com.project.SEP_BE_EmployeeManagement.dto.UserAttendance;
 import com.project.SEP_BE_EmployeeManagement.dto.request.attendance.EditAttendance;
+import com.project.SEP_BE_EmployeeManagement.dto.request.attendance.UpdateSignInAttendanceRequest;
 import com.project.SEP_BE_EmployeeManagement.dto.response.MessageResponse;
+import com.project.SEP_BE_EmployeeManagement.dto.response.attendance.AttendanceResponse;
 import com.project.SEP_BE_EmployeeManagement.model.Attendance;
 import com.project.SEP_BE_EmployeeManagement.model.User;
 import com.project.SEP_BE_EmployeeManagement.repository.AttendanceRepository;
 import com.project.SEP_BE_EmployeeManagement.repository.UserRepository;
 import com.project.SEP_BE_EmployeeManagement.service.AttendanceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -114,5 +119,35 @@ public class AttendanceController {
     @PostMapping("edit")
     public ResponseEntity<MessageResponse> updateLogSign(@Valid @RequestBody EditAttendance[] editAttendances) throws MessagingException, UnsupportedEncodingException {
         return ResponseEntity.ok(attendanceService.updateAttendance(editAttendances));
+    }
+    @GetMapping("/get-list-attendance")
+    public ResponseEntity<?> getList(@RequestParam(name = "page", defaultValue = "0") int page,
+                                     @RequestParam(name = "size", defaultValue = "30") int size,
+                                     @RequestParam(name = "departmentId", defaultValue = "") String departmentId,
+                                     @RequestParam(name = "from", defaultValue = "",required = false) String fromDate,
+                                     @RequestParam(name = "to", defaultValue = "",required = false) String toDate) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<AttendanceResponse> pageRequests = attendanceService.getList( departmentId, fromDate, toDate, pageable);
+        return ResponseEntity.ok(pageRequests);
+    }
+
+    @GetMapping("/get-list-attendance-by-user-id")
+    public ResponseEntity<?> getListByUserId(@RequestParam(name = "page", defaultValue = "0") int page,
+                                             @RequestParam(name = "size", defaultValue = "30") int size,
+                                             @RequestParam(name = "from", defaultValue = "",required = false) String fromDate,
+                                             @RequestParam(name = "to", defaultValue = "",required = false) String toDate) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<AttendanceResponse> pageRequests = attendanceService.getListByUserId(fromDate, toDate, pageable);
+        return ResponseEntity.ok(pageRequests);
+    }
+
+    @PutMapping("/update-signs/{attendanceId}")
+    public ResponseEntity<?> updateSigns(@RequestBody UpdateSignInAttendanceRequest request, @PathVariable Long attendanceId){
+        Attendance obj = attendanceService.getByAttendanceId(attendanceId);
+        if(obj == null){
+            return ResponseEntity.internalServerError().body("Đã xảy ra lỗi: Không tồn tại chấm công này.");
+        }
+        attendanceService.updateSigns(request.getSign(), attendanceId, request.getReason());
+        return ResponseEntity.ok(obj);
     }
 }
